d9a28a7b675dec9befd5525e1b2000bb25e4b28f
Lucas Liu <lucasliu0910@gmail.com>
2026-09-16 16:34:47 +0000

    Upadate

---
+++ 20260916.R
@@ -0,0 +1,58 @@
+R.version.string
+
+2 + 3
+
+sqrt(4)
+
+variable1 = 1 + 1
+variable1
+
+variable2 = sqrt(2)
+variable2
+
+vector1 = 1:9
+vector1
+
+vector2 = c(9:1)
+vector2
+
+vector3 = c(2.1, 3.6, 5.8, 7.9)
+vector3
+length(vector3)
+
+name = "Lucas"
+name
+
+x = c(1:4)
+mean(x)
+sum(x)
+sum(x - 1)
+sum(x - mean(x))
+x[1]
+
+mean(c(T,T,F,F))
+
+(1 >= 2)
+(1 >= 2) | (2 >= 1)
+
+y = c(4, 8, NA, 7)
+mean(y)
+mean(y, na.rm = TRUE)
+
+vector3 = c(2.1, 3.6, 5.8, 7.9)
+matrix1 = matrix(vector3, nrow = 2, ncol = 2)
+matrix1
+dim(matrix1)
+
+matrix1 + matrix1
+matrix1 %*% matrix1
+
+t(matrix1) # transpose of a matrix
+solve(matrix1) # inverse of a matrix
+
+matrix1[1, 2]
+
+
+
+
+
